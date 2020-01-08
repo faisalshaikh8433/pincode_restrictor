ea80a7a70f23116474cdc50cd4f2e5d0b6c10f14
faisal <faisalshaikh8433@gmail.com>
2020-01-08 19:52:24 +0530

    updated restricter.js

--- restricter.js
+++ restricter.js
@@ -1,1 +1,14 @@
-console.log('hello')
+var elements =
+  "<div style='margin-top: 1rem;'>" +
+  "<strong style='color: lightcoral; padding: 2px;'>" +
+  "Please enter the pincode to verify we deliver in your area or not !" +
+  "</strong>" +
+  "</div>" +
+  "<div style='margin-top: 1rem;'>" +
+  "<input type='text' id='pincode' placeholder='pincode' autofocus />" +
+  "<input type='button' class='btn' value='Verify' id='verify_pincode' />" +
+  "</div>";
+// var mainElement = document.getElementById("shopify-section-cart-template");
+var parser = new DOMParser();
+var html = parser.parseFromString(elements, "text/html");
+document.querySelector("body").append(elements);
