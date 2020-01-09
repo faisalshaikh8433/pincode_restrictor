10bacc3951ffbae6e895ce67f72235f783097070
faisal <faisalshaikh8433@gmail.com>
2020-01-09 19:23:31 +0530

    updates restricter.js

--- restricter.js
+++ restricter.js
@@ -1,14 +1,6 @@
 var elements =
-  "<div style='margin-top: 1rem;'>" +
-  "<strong style='color: lightcoral; padding: 2px;'>" +
-  "Please enter the pincode to verify we deliver in your area or not !" +
-  "</strong>" +
-  "</div>" +
-  "<div style='margin-top: 1rem;'>" +
-  "<input type='text' id='pincode' placeholder='pincode' autofocus />" +
-  "<input type='button' class='btn' value='Verify' id='verify_pincode' />" +
-  "</div>";
-// var mainElement = document.getElementById("shopify-section-cart-template");
+  "<div style='margin-top: 1rem;'><strong style='color: lightcoral; padding: 2px;'>Please enter the pincode to verify we deliver in your area or not !</strong></div><div style='margin-top: 1rem;'><input type='text' id='pincode' placeholder='pincode' autofocus /><input type='button' class='btn' value='Verify' id='verify_pincode' /></div>";
 var parser = new DOMParser();
+// var mainElement = document.getElementById("shopify-section-cart-template");
 var html = parser.parseFromString(elements, "text/html");
-document.querySelector("body").append(elements);
+document.querySelector("body").append(html);
